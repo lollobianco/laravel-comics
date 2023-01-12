18ed41318ee64f70c2f1fb83587e393f491dbaeb
lollobianco <loreb2002@gmail.com>
2023-01-12 16:59:17 +0100

    Header completato

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,36 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title> @yield('page-title') </title>
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-</head>
-<body>
-   
 
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+  <header>
+    @include('partials.header')
+  </header>
+  <main>
+
+  </main>
+  <footer>
+    @include('partials.footer')
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
 </body>
+
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<div class="blue-section">
+
+   <div class="container d-flex align-items-center justify-content-end">
+
+      <span class="fs-xxs mx-2">DC POWER VISA@</span>
+      <span class="fs-xxs mx-2">ADDITIONAL DC SITES</span>
+
+   </div>
+
+</div>
+
+<nav class="container d-flex justify-content-center">
+   <ul class="d-flex align-items-center m-0 p-3">
+      <li class="mx-3 list-unstyled fw-bold"> <img src="{{asset('images/dc-logo.png')}}" alt="logo-dc"> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">CHARACTER</a> </li>
+      <li class="mx-3 list-unstyled fw-bold active"> <a href="">COMICS</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">MOVIES</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">TV</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">GAMES</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">COLLECTIBLES</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">VIDEOS</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">FANS</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">NEWS</a> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="">SHOP</a> </li>
+   </ul>
+</nav>
+
+<div class="jumbo-container"></div>
