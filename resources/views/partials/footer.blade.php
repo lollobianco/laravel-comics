5dd99a4ec34448d17510d8d02f9fe4d9d2b16586
lollobianco <loreb2002@gmail.com>
2023-01-12 19:05:45 +0100

    Completato footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,74 @@
-@extends('layouts.app')
+<div id="bg-container">
+   <div id="sized-container">
+      <div class="flex">
+         <div>
+            <ul>
+               <li class="title">DC COMICS</li>
+               <li>Characters</li>
+               <li>Comics</li>
+               <li>Movies</li>
+               <li>TV</li>
+               <li>Games</li>
+               <li>Videos</li>
+               <li>News</li>
+            </ul>
+            <ul>
+               <li class="title">SHOP</li>
+               <li>Shop DC</li>
+               <li>Shop DC Collectibles</li>
+            </ul>
+         </div>
+         <div>
+            <ul>
+               <li class="title">DC</li>
+               <li>Therms Of Use</li>
+               <li>Privacy policy (New)</li>
+               <li>Ad Choises</li>
+               <li>Advertising</li>
+               <li>Jobs</li>
+               <li>Subscriptions</li>
+               <li>Talent Workshops</li>
+               <li>CPSC Certificates</li>
+               <li>Ratings</li>
+               <li>Shop Help</li>
+               <li>Contact Us</li>
+            </ul>
+         </div>
+         <div>
+            <ul>
+               <li class="title">SITES</li>
+               <li>DC</li>
+               <li>MAD Magazine</li>
+               <li>DC Kids</li>
+               <li>DC Univese</li>
+               <li>DC Power Visa</li>
+            </ul>
+         </div>
+      </div>
+
+      <div class="img-container">
+        
+      </div>
+      
+   </div>
+
+   <div id="footer-comp">
+      <div id="footer-container">
+
+         <div>
+            <button class="button">SIGN-UP NOW</button>
+         </div>
+
+         <div id="follow-us">
+            <div class="follow-us-text">FOLLOW US</div>
+            <img src="{{asset('images/footer-facebook.png')}}" alt="logo" class="logo">
+            <img src="{{asset('images/footer-twitter.png')}}" alt="logo" class="logo">
+            <img src="{{asset('images/footer-youtube.png')}}" alt="logo" class="logo">
+            <img src="{{asset('images/footer-pinterest.png')}}" alt="logo" class="logo">
+            <img src="{{asset('images/footer-periscope.png')}}" alt="logo" class="logo">
+         </div>
+
+      </div>
+   </div>
+
+</div>
