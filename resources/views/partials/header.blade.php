e0bf674dc8e69b09fc7f3c4a0faed9cb17fe84ae
lollobianco <loreb2002@gmail.com>
2023-01-13 12:37:29 +0100

    Fatte piccole modifiche

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,9 +11,9 @@
 
 <nav class="container d-flex justify-content-center">
    <ul class="d-flex align-items-center m-0 p-3">
-      <li class="mx-3 list-unstyled fw-bold"> <img src="{{asset('images/dc-logo.png')}}" alt="logo-dc"> </li>
+      <li class="mx-3 list-unstyled fw-bold"> <a href="{{route('home')}}"> <img src="{{asset('images/dc-logo.png')}}" alt="logo-dc"> </a> </li>
       <li class="mx-3 list-unstyled fw-bold"> <a href="">CHARACTER</a> </li>
-      <li class="mx-3 list-unstyled fw-bold active"> <a href="/">COMICS</a> </li>
+      <li class="mx-3 list-unstyled fw-bold active"> <a href="{{route('home')}}">COMICS</a> </li>
       <li class="mx-3 list-unstyled fw-bold"> <a href="">MOVIES</a> </li>
       <li class="mx-3 list-unstyled fw-bold"> <a href="">TV</a> </li>
       <li class="mx-3 list-unstyled fw-bold"> <a href="">GAMES</a> </li>
